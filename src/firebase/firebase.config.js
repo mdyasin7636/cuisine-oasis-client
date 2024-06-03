e7f24e51153b193f6d4a64f98c3e853c2d3d5114
mdyasin7636 <mdyasin7636@gmail.com>
2024-06-03 11:06:11 +0600

    updated

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCriehqTSZfp1DaFRHmyYhtJIAwa1WcHkE",
-  authDomain: "cuisine-oasis.firebaseapp.com",
-  projectId: "cuisine-oasis",
-  storageBucket: "cuisine-oasis.appspot.com",
-  messagingSenderId: "178345489076",
-  appId: "1:178345489076:web:31f9ccd55c56e12e541de1",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 export const app = initializeApp(firebaseConfig);
